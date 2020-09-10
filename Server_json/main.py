af01d823928e3e519c055bb0ddd7c31ff76a2ca3
YangBH-0 <67933557+YangBH-0@users.noreply.github.com>
2020-09-10 15:47:09 +0900

    Add files via upload
    오류들 수정

--- Server_json/main.py
+++ Server_json/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 import TcpServer
@@ -13,25 +13,14 @@ with open(file_path, "r") as fj:
     a = fd['PORT']
     b = fd['DB_Host']
     c = fd['DB_User']
-    d = fd['DB_name']
-    e = fd['DB_password'] 
+    d = fd['DB_password'] 
+    e = fd['DB_name']
     f = fd['Specific_table_name']
     g = fd['Device_Register_table_name']
+    print(a,b,c,d,e,f,g);
 server=TcpServer.TcpServer(a,b,c,d,e,f,g)
 server.DB_Con()
 
 while True:
     server.run()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
